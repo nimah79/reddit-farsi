2bbd77d1e4931200e7842fb277e7fd3d4dc9f53f
Nima HeydariNasab <nima.heydari79@yahoo.com>
2022-01-28 17:24:58 +0330

    Add comment replies relationship

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -18,4 +18,9 @@ class Comment extends Model
     {
         return $this->belongsTo(Post::class);
     }
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
 }
