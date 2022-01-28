8e63e0c77420a6fa5e31aba7fc3332ac8b9ca42b
Nima HeydariNasab <nima.heydari79@yahoo.com>
2022-01-28 16:58:10 +0330

    Add community creator and change user name to username

--- app/Models/Community.php
+++ app/Models/Community.php
@@ -9,6 +9,11 @@ class Community extends Model
 {
     use HasFactory;
 
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+
     public function users()
     {
         return $this->belongsToMany(User::class);
